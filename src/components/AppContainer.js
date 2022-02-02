c7a1f26ac22b54d080852fa8605f41f44afdc6b0
Francine Lima <94610559+francine1919@users.noreply.github.com>
2022-02-02 16:29:21 -0300

    Update AppContainer.js

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,64 +1,64 @@
-import React, { Component } from 'react'
-import { ShoppingCart } from './ShoppingCart/ShoppingCart'
+// import React, { Component } from 'react'
+// import { ShoppingCart } from './ShoppingCart/ShoppingCart'
 
-export class AppContainer extends Component {
+// export class AppContainer extends Component {
   
-  state = {
+//   state = {
 
-  }
+//   }
 
 
 
-  onAddProductToCart = productId =>{
-    const productInCart = this.state.productsInCart.find(product => productId === product.id)
+//   onAddProductToCart = productId =>{
+//     const productInCart = this.state.productsInCart.find(product => productId === product.id)
 
-    if(productInCart){
-      const newProductsInCart = this.state.productsInCart.map(product => {
-        if(productId === product.id) {
-          return {
-            ...product,
-            quantity = product.quantity + 1
-          }
-        }
+//     if(productInCart){
+//       const newProductsInCart = this.state.productsInCart.map(product => {
+//         if(productId === product.id) {
+//           return {
+//             ...product,
+//             quantity = product.quantity + 1
+//           }
+//         }
 
-        return product 
-      })
+//         return product 
+//       })
 
-      this.setState ({produInCart: newProductsInCart})
-    } else{
-      const productToAdd = products.find(product => productId === product.id)
+//       this.setState ({produInCart: newProductsInCart})
+//     } else{
+//       const productToAdd = products.find(product => productId === product.id)
 
-      const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1} ]
+//       const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1} ]
 
-      this.setState({productdInCart: newProductsInCart})
-    }
-  }
+//       this.setState({productdInCart: newProductsInCart})
+//     }
+//   }
   
 
-  onRemoveProductFromCart = (productId) => {
-    const newProductsInCart = this.state.productsInCart.map((product) =>{
-      if(product.id === productId) {
-        return{
-          ...product,
-          quantity: product.quantity - 1
-        }
-      }
-      return product
-    }).filter((product) => product.quantity > 0)
+//   onRemoveProductFromCart = (productId) => {
+//     const newProductsInCart = this.state.productsInCart.map((product) =>{
+//       if(product.id === productId) {
+//         return{
+//           ...product,
+//           quantity: product.quantity - 1
+//         }
+//       }
+//       return product
+//     }).filter((product) => product.quantity > 0)
 
-    this.setState({productsInCart: newProductsInCart})
-  }
+//     this.setState({productsInCart: newProductsInCart})
+//   }
 
   
   
-  render() {
-    return (
-      <AppContainer>
-      <ShoppingCart
-      productsInCart = {this.state.productsInCart}
-      onRemoveProductFromCart = {this.onRemoveProductFromCart}
-      />
-      </AppContainer>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <AppContainer>
+//       <ShoppingCart
+//       productsInCart = {this.state.productsInCart}
+//       onRemoveProductFromCart = {this.onRemoveProductFromCart}
+//       />
+//       </AppContainer>
+//     );
+//   }
+// }
